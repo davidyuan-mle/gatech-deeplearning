c89c626b315004afdb0879d9320e290ed2182301
Chengwei "David" Yuan <yystats@gmail.com>
2025-06-01 01:10:35 -0700

    add assignment 1

---
+++ assignment1/models/two_layer_nn.py
@@ -0,0 +1,151 @@
+""" 			  		 			     			  	   		   	  			  	
+MLP Model.  (c) 2021 Georgia Tech
+
+Copyright 2021, Georgia Institute of Technology (Georgia Tech)
+Atlanta, Georgia 30332
+All Rights Reserved
+
+Template code for CS 7643 Deep Learning
+
+Georgia Tech asserts copyright ownership of this template and all derivative
+works, including solutions to the projects assigned in this course. Students
+and other users of this template code are advised not to share it with others
+or to make it available on publicly viewable websites including repositories
+such as Github, Bitbucket, and Gitlab.  This copyright statement should
+not be removed or edited.
+
+Sharing solutions with current or future students of CS 7643 Deep Learning is
+prohibited and subject to being investigated as a GT honor code violation.
+
+-----do not edit anything above this line---
+"""
+
+# Do not use packages that are not in standard distribution of python
+import numpy as np
+
+np.random.seed(1024)
+from ._base_network import _baseNetwork
+
+
+class TwoLayerNet(_baseNetwork):
+    def __init__(self, input_size=28 * 28, num_classes=10, hidden_size=128):
+        super().__init__(input_size, num_classes)
+
+        self.hidden_size = hidden_size
+        self._weight_init()
+
+    def _weight_init(self):
+        """
+        initialize weights of the network
+        :return: None; self.weights is filled based on method
+        - W1: The weight matrix of the first layer of shape (num_features, hidden_size)
+        - b1: The bias term of the first layer of shape (hidden_size,)
+        - W2: The weight matrix of the second layer of shape (hidden_size, num_classes)
+        - b2: The bias term of the second layer of shape (num_classes,)
+        """
+
+        # initialize weights
+        self.weights['b1'] = np.zeros(self.hidden_size)
+        self.weights['b2'] = np.zeros(self.num_classes)
+        np.random.seed(1024)
+        self.weights['W1'] = 0.001 * np.random.randn(self.input_size, self.hidden_size)
+        np.random.seed(1024)
+        self.weights['W2'] = 0.001 * np.random.randn(self.hidden_size, self.num_classes)
+
+        # initialize gradients to zeros
+        self.gradients['W1'] = np.zeros((self.input_size, self.hidden_size))
+        self.gradients['b1'] = np.zeros(self.hidden_size)
+        self.gradients['W2'] = np.zeros((self.hidden_size, self.num_classes))
+        self.gradients['b2'] = np.zeros(self.num_classes)
+
+    def forward(self, X, y, mode='train'):
+        """
+        The forward pass of the two-layer net. The activation function used in between the two layers is sigmoid, which
+        is to be implemented in self.,sigmoid.
+        The method forward should compute the loss of input batch X and gradients of each weights.
+        Further, it should also compute the accuracy of given batch. The loss and
+        accuracy are returned by the method and gradients are stored in self.gradients
+
+        :param X: a batch of images (N, input_size)
+        :param y: labels of images in the batch (N,)
+        :param mode: if mode is training, compute and update gradients;else, just return the loss and accuracy
+        :return:
+            loss: the loss associated with the batch
+            accuracy: the accuracy of the batch
+            self.gradients: gradients are not explicitly returned but rather updated in the class member self.gradients
+        """
+        loss = None
+        accuracy = None
+        #############################################################################
+        # TODO:                                                                     #
+        #    1) Implement the forward process:                                      #
+        #        1) Call sigmoid function between the two layers for non-linearity  #
+        #        2) The output of the second layer should be passed to softmax      #
+        #        function before computing the cross entropy loss                   #
+        #    2) Compute Cross-Entropy Loss and batch accuracy based on network      #
+        #       outputs                                                             #
+        #############################################################################
+        W1 = self.weights['W1']
+        b1 = self.weights['b1']
+        W2 = self.weights['W2']
+        b2 = self.weights['b2']
+        N = X.shape[0]
+
+        # Forward pass
+        layer1 = X @ W1 + b1                        # (N, hidden_size)
+        layer1_sigmoid = self.sigmoid(layer1)       # (N, hidden_size)
+        scores = layer1_sigmoid @ W2 + b2           # (N, num_classes)
+        prob = self.softmax(scores)                 # (N, num_classes)
+        loss = self.cross_entropy_loss(prob, y) 
+        accuracy = self.compute_accuracy(prob, y)
+
+        if mode != 'train':
+            return loss, accuracy
+
+
+        #############################################################################
+        #                              END OF YOUR CODE                             #
+        #############################################################################
+
+        #############################################################################
+        # TODO:                                                                     #
+        #    1) Implement the backward process:                                     #
+        #        1) Compute gradients of each weight and bias by chain rule         #
+        #        2) Store the gradients in self.gradients                           #
+        #    HINT: You will need to compute gradients backwards, i.e, compute       #
+        #          gradients of W2 and b2 first, then compute it for W1 and b1      #
+        #          You may also want to implement the analytical derivative of      #
+        #          the sigmoid function in self.sigmoid_dev first                   #
+        #############################################################################
+
+        # Backward pass
+
+        # derivative of L to scores 
+        d_scores = prob.copy()                          # (N, num_classes)
+        d_scores[np.arange(N), y] -= 1                  # (N, num_classes)
+        d_scores /= N                                   # (N, num_classes)
+        
+        # derivative of L to W2 and b2 
+        d_W2 = layer1_sigmoid.T @ d_scores              # (hidden_size, num_classes)
+        d_b2 = np.sum(d_scores, axis=0)      
+        self.gradients['W2'] = d_W2
+        self.gradients['b2'] = d_b2
+
+        # derivative of L to layer1_sigmoid
+        d_layer1_sigmoid = d_scores @ W2.T              # (N, hidden_size)
+        
+        # derivative of layer1_sigmoid to layer1
+        d_layer1 = d_layer1_sigmoid * self.sigmoid_dev(layer1)  # (N, hidden_size)
+       
+        # derivative of L to W1 and b1
+        d_W1 = X.T @ d_layer1                           # (input_size, hidden_size)
+        d_b1 = np.sum(d_layer1, axis=0)
+        self.gradients['W1'] = d_W1
+        self.gradients['b1'] = d_b1
+
+
+        #############################################################################
+        #                              END OF YOUR CODE                             #
+        #############################################################################
+
+        return loss, accuracy
